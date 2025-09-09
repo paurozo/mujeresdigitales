b9ffdde8904f00522556b57ecc11947cfba77a77
Paula Rozo <paularozo609@gmail.com>
2025-09-09 07:01:56 -0500

    creacion de ejercicio par_impar

--- js/tablas multiplicar.js
+++ js/tablas multiplicar.js
@@ -1,9 +1,15 @@
-for (let multiplicando = 1; multiplicando <= 10; multiplicando++) {
-    console.log(`Número ${multiplicando}`);  
+//for (let multiplicando = 1; multiplicando <= 10; multiplicando++) {
+  //  console.log(`Número ${multiplicando}`);  
 
-    let multiplicador = 1;
-    while ( multiplicador <= 10) {
-        console.log(`${multiplicando} x ${multiplicador} = ${multiplicando * multiplicador}`);
-        multiplicador++;
-    }
-}
+    //let multiplicador = 1;
+    //while ( multiplicador <= 10) {
+      //  console.log(`${multiplicando} x ${multiplicador} = ${multiplicando * multiplicador}`);
+        //multiplicador++;
+    //}
+
+
+
+for table_number in rangue(1,10);
+ print (f"tabla del {table_number}")
+  for number in range(1,10):
+  print (f"{number}x{table number}"={number * table_number})
