93bf2e46336a417b48dff7f40ed09e503b27f86b
Paula Rozo <paularozo609@gmail.com>
2025-09-09 08:05:07 -0500

    Ajuste actividad tablas de multiplicar con ciclo

---
+++ js/tablasmultiplicar.js
@@ -0,0 +1,11 @@
+for (let table_number = 1; table_number < 10; table_number++) {
+  console.log();
+  console.log('Tabla del '+ table_number);
+  for (let number = 1; number < 11; number++) {
+    let total = number * table_number;
+    console.log(number + ' x ' + table_number + ' = ' + total );
+  }
+  console.log(); 
+}
+
+
